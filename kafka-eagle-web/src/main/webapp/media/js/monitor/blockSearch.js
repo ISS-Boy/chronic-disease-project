7bc2375bdbf7421578f76b0367c539832ab4ec3f
L1116 <976130720@qq.com>
2018-04-30 20:36:56 +0800

    修改部分代码,组内过滤减为零时移除边框

--- kafka-eagle-web/src/main/webapp/media/js/monitor/blockSearch.js
+++ kafka-eagle-web/src/main/webapp/media/js/monitor/blockSearch.js
@@ -50,18 +50,27 @@
     function add_aggregation_predicate(span) {
         var this_block = $(span).parents('.block_tmpl')
         if(!checkTopicEmpty(this_block))
-            return
+            return;
+        var aggregations = $(span).parents('.block_tmpl').find($('.aggregation_group')).children();
+        var length = aggregations.length;
+        console.log(length);
+        if(length==0)
+            $('.aggregation_group').css("border","1px solid #eee");
         var aggregation_group = $(span).parents('.aggregation_group_panel').find('.aggregation_group');
         $('#calculationTmpl').tmpl().appendTo(aggregation_group);
         $('.selectpicker').selectpicker({
         'selectedText': 'cat'  
         });
-
         // 为input添加响应事件
         addListenerForBlock(this_block, '.aggregation_group', 'g_source')
     }
 
     function remove_aggregation_predicate(span) {
+        var aggregations = $(span).parents('.block_tmpl').find($('.aggregation_group')).children();
+        var length = aggregations.length;
+        console.log(length);
+        if (length < 2)
+            $('.aggregation_group').css("border","none");
         //  var aggregations = $(span).parents('.block_tmpl').find($('.aggregation_group')).children();
         // if (aggregations.length>1)
         $(span).parents('.aggregation_group_templ').remove();
